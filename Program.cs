6787e348f0eeb26acd9c079e73e4e545b508e2a0
Idowu Akinde <8018104+idowuakinde@users.noreply.github.com>
2018-12-08 17:34:12 +0100

    Example ASP.NET Core 2.1 Console App, based on Udemy tutorial...

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("New Message");
+        }
+    }
+}
